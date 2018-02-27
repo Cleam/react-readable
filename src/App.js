a3619effb30852c79d422635b47cbef3316e7015
lizhigaomac <lizhigao@021.com>
2018-02-27 23:33:20 +0800

    接口调通

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { serverUrl } from "./config";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backend: 'backend-data'
+    }
+  }
+
+  componentDidMount() {
+    console.log(process.env);
+    const url = `${process.env.REACT_APP_BACKEND}/categories`;
+    console.log('fetching from url', url);
+    fetch(url, { headers: { 'Authorization': 'whatever-you-want' },
+                 credentials: 'include' } )
+      .then( (res) => { return(res.text()) })
+      .then((data) => {
+        this.setState({backend:data});
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Talking to the backend yields these categories: <br/>
+          {this.state.backend}
         </p>
       </div>
     );
